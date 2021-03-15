c71758b6b104d4785456e0f3f023f3796bee48f7
meti651 <meti651@gmail.com>
2021-03-15 22:59:39 +0100

    initialize vue material to vue app

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,17 @@
-// Application entry point
-// Write your own code here
-document.getElementById('app').innerHTML = '<pre>IdomSoft test application is now running!</pre>'
+import Vue from "vue"
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import "./assets/style.scss"
+import App from "./App.vue";
+
+import VueMaterial from "vue-material"
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+new Vue({
+    el: '#app',
+    components: {App},
+    template: '<App/>'
+})
