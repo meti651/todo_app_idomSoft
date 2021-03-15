53a4dc2b41c16e7656a32950b73d7277807e906a
meti651 <meti651@gmail.com>
2021-03-15 23:02:45 +0100

    App component: with dummy data

---
+++ client/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <DatedCard :todos="todos">
+        <template #children>
+            <TodoList :todos="todos"/>
+        </template>
+    </DatedCard>
+</template>
+
+<script>
+import DatedCard from "./components/DatedCard"
+import TodoList from "./components/TodoList"
+import TodoUpdate from "./components/TodoUpdate"
+
+export default {
+    name: "App",
+    components: {
+        DatedCard,
+        TodoList,
+        TodoUpdate
+    },
+    data: () => ({
+        todos: [],
+    }),
+    mounted() {
+            this.todos = [
+                {
+                    id: 1,
+                    description: "Buy Pizza",
+                    time: "10:00AM"
+                },
+                {
+                    id: 2,
+                    description: "Run",
+                    time: "11:00AM"
+                },
+                {
+                    id: 3,
+                    description: "Walk Dog",
+                    time: "12:00AM"
+                },
+                {
+                    id: 4,
+                    description: "Lunch",
+                    time: "13:00AM"
+                }
+            ]
+            
+        }
+}
+</script>
