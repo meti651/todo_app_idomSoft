3c7be11b1ad230b6da0e5b4acf920ed4bc6982e8
meti651 <meti651@gmail.com>
2021-03-15 23:07:11 +0100

    TodoList component

---
+++ client/src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <md-list>
+            <div v-for="todo, index in todos" :key="todo.id" :class="index % 2 === 0 && 'grayed'">
+                <Todo :todo="todo"/>
+            </div>
+        </md-list>
+    </div>
+</template>
+
+<script>
+import Todo from "./Todo"
+
+export default {
+    name: "TodoList",
+    components: {
+        Todo
+    },
+    props: {
+        todos: Array,
+    },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .grayed{
+        background-color: rgb(250, 250, 250);
+    }
+</style>
+
