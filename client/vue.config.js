7f46c8625b5a7f33d7b90d7fa399465ae70f5239
meti651 <meti651@gmail.com>
2021-03-19 16:51:47 +0100

    webpack-bundler-analyzer configuration

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,15 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
+    configureWebpack: { plugins: [new BundleAnalyzerPlugin()] },
     runtimeCompiler: true,
     css: {
         loaderOptions: {
             scss: {
                 prependData: `
                 @import "@/assets/_config.scss";
-                `
-            }
-        }
-    }
-}
+                `,
+            },
+        },
+    },
+};
